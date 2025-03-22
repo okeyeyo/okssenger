d60af01e147b979bf22eb7ebd85c75419ae72044
okeyeyo <clskfk1234@naver.com>
2025-03-22 19:24:15 +0900

    class로 통일

--- javascript/battery.js
+++ javascript/battery.js
@@ -1,8 +1,8 @@
 if ("getBattery" in navigator) {
   navigator.getBattery().then((battery) => {
-    const batterySpan = document.getElementById("battery");
+    const batterySpan = document.getElementsByClassName("battery")[0];
     batterySpan.textContent = `${battery.level * 100}%`;
   });
 } else {
-  console.log("Battery API is not supported on this browser.");
+  console.log("Battery API 지원하지 않음.");
 }
