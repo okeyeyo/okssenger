c2a6226592a1755206c05a9b75dd18ffb124aaa4
okeyeyo <clskfk1234@naver.com>
2025-04-27 23:27:11 +0900

    gitignore에 node_modules 추가 및 언트랙 처리

--- README.md
+++
@@ -1,2 +0,0 @@
-It's to make a clone of messenger program
-use html, css, js
--- okssenger-front/src/utils/loading.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener("load", function () {
-  document.getElementById("loading-screen").style.display = "none";
-  this.document.getElementById("main-content").style.display = "block";
-});
