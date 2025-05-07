6f29fcb76bf9256f7d8b441794682d0d1ffb333a
okeyeyo <clskfk1234@naver.com>
2025-05-07 22:55:50 +0900

    chats 비율 수정

--- okssenger-front/src/pages/Chats.jsx
+++ okssenger-front/src/pages/Chats.jsx
@@ -4,33 +4,33 @@ import "../styles/App.css"; // 스타일 파일 import
 
 function Main() {
   return (
-    <div>
+    <div className="main-screen">
       <header className="screen-header">
-        <h1 className="screen-header__title">대화</h1>
+        <h3 className="screen-header__title">친구</h3>
         <div className="screen-header__icons">
           <span>
-            <FaSearch />
+            <FaSearch size={28} />
           </span>
           <span>
-            <FaComment />
+            <FaComment size={28} />
           </span>
           <span>
-            <FaCog />
+            <FaCog size={28} />
           </span>
         </div>
       </header>
 
-      <main className="main-screen">
-        <a href="chat.html">
+      <main>
+        <a className="link" href="chat.html">
           <div className="user-component">
             <div className="user-component__column">
               <img
                 src="https://data.onnada.com/character/202503/thumb_1994052142_31a1e082__main1.png"
-                className="user-component__avatar"
+                className="user-component__avatar  user-component__avatar--sm"
                 alt="User Avatar"
               />
-              <div className="user-component__text">
-                <h4 className="user-component__name">坂本太郎</h4>
+              <div className="user-component__info">
+                <h4 className="user-component__info-name">坂本太郎</h4>
                 <h6 className="user-component__introduce">
                   キムさん、お誕生日おめでとうございます。
                 </h6>
